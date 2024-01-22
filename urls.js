e81e636369ee36ee259dd01df79047d0a8acaf4d
muhammedkoramaz <muhammedkoramaz@gmail.com>
2024-01-22 04:52:40 +0300

    url export.

---
+++ urls.js
@@ -0,0 +1,75 @@
+export const API_KEY =
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjAxMDcyZjYxMTEyNWYzODUyYmZhMmY2ODQzZmNhYiIsInN1YiI6IjYyMTI0NjEyM2Y0ODMzMDAxYzk1ZDEzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PFFarWR4jNmnkrLt0ZH428RZugAjHGOlLEpWKkBxBnk";
+export const SHORT_API_KEY = "df01072f611125f3852bfa2f6843fcab";
+
+// TRENDING
+export const TRENDING_ALL_DAY =
+  "https://api.themoviedb.org/3/trending/all/day?language=en-US&page=1";
+export const TRENDING_MOVIES_DAY =
+  "https://api.themoviedb.org/3/trending/movie/day?language=en-US&page=1";
+export const TRENDING_TV_DAY =
+  "https://api.themoviedb.org/3/trending/tv/day?language=en-US&page=1";
+export const TRENDING_PEOPLE_DAY =
+  "https://api.themoviedb.org/3/trending/person/day?language=en-US&page=1";
+export const TRENDING_ALL_WEEK =
+  "https://api.themoviedb.org/3/trending/all/week?language=en-US&page=1";
+export const TRENDING_MOVIES_WEEK =
+  "https://api.themoviedb.org/3/trending/movie/week?language=en-US&page=1";
+export const TRENDING_TV_WEEK =
+  "https://api.themoviedb.org/3/trending/tv/week?language=en-US&page=1";
+export const TRENDING_PEOPLE_WEEK =
+  "https://api.themoviedb.org/3/trending/person/week?language=en-US&page=1";
+
+// MOVIES
+export const POPULAR_MOVIES =
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+export const TREND_MOVIES =
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+export const UPCOMING_MOVIES =
+  "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1";
+export const TOP_RATED_MOVIES =
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+export const NOW_PLAYING_MOVIES =
+  "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
+
+// TV SHOWS
+export const POPULAR_TV =
+  "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1";
+export const TREND_TV =
+  "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1";
+export const TOP_RATED_TV =
+  "https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1";
+export const ON_THE_AIR_TV =
+  "https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=1";
+export const AIRING_TODAY_TV =
+  "https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=1";
+
+// PEOPLE
+export const POPULAR_PEOPLE =
+  "https://api.themoviedb.org/3/person/popular?language=en-US&page=1";
+
+// SEARCH
+export const SEARCH_MOVIES =
+  "https://api.themoviedb.org/3/search/movie?language=en-US&page=1&include_adult=false";
+export const SEARCH_TV =
+  "https://api.themoviedb.org/3/search/tv?language=en-US&page=1&include_adult=false";
+export const SEARCH_PEOPLE =
+  "https://api.themoviedb.org/3/search/person?language=en-US&page=1&include_adult=false";
+export const SEARCH_ALL =
+  "https://api.themoviedb.org/3/search/multi?language=en-US&page=1&include_adult=false";
+
+// WHATS POPULAR
+export const STREAMING =
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&watch_region=TR&with_watch_monetization_types=flatrate";
+export const ON_TV =
+  "https://api.themoviedb.org/3/discover/tv?include_adult=false&watch_region=TR&include_null_first_air_dates=false&page=1&sort_by=popularity.desc";
+export const FOR_RENT =
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&watch_region=TR&with_watch_monetization_types=rent";
+export const IN_THEATERS =
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&watch_region=TR&with_watch_monetization_types=buy";
+
+// FOR FREE
+export const FREE_MOVIES =
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&watch_region=TR&with_watch_monetization_types=free";
+export const FREE_TV =
+  "https://api.themoviedb.org/3/discover/tv?include_adult=false&watch_region=TR&include_null_first_air_dates=false&page=1&sort_by=popularity.desc&with_watch_monetization_types=free";
