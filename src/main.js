ec0d7e341eb92205afdfc4da87a370d0f42782f0
muhammedkoramaz <muhammedkoramaz@gmail.com>
2024-01-22 04:53:19 +0300

    some fixed.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueYoutube from "vue-youtube";
 
+Vue.use(VueYoutube);
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   router,
-}).$mount('#app');
+  store
+}).$mount("#app");
